fedb4a5e7c6edc50d16d11a7e8657df25454f38a
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-03-21 12:07:28 +0100

    start to implement exercise 03

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -1,8 +1,20 @@
 package u04lab.polyglot.a01a
 import Logics.*
+import scala.util.Random
 
-/** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
+/** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/
+ * description: https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/Test.java*/
 class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
+  private val FAILURES = 5
+  private var failures = 0
+  private val random = Random(42)
+  private val boatRow = random.nextInt(size)
+  private val boatLeftCol = random.nextInt(size-boat+1)
+
+  println(s"x=$boatLeftCol  y=$boatRow") //x=0 y=2
 
   def hit(row: Int, col: Int) =
-    Result.HIT
+    if row == boatRow && col == boatLeftCol
+      then Result.HIT
+    else
+      Result.MISS
