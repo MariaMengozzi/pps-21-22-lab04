961c2346a6d3ed6fd41f6df8323ac2de65589c47
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-03-21 11:29:23 +0100

    implement exercise 2 about students and curses

--- src/main/scala/u04lab/code/Student.scala
+++ src/main/scala/u04lab/code/Student.scala
@@ -14,10 +14,18 @@ trait Course:
   def teacher: String
 
 object Student:
-  def apply(name: String, year: Int = 2017): Student = ???
+  def apply(name: String, year: Int = 2017): Student = StudentImpl(name, year)
+  private case class StudentImpl(override val name: String,
+                                 override val year: Int) extends Student:
+
+    private var _courses: List[Course] = Nil()
+    override def courses: List[String] = map(_courses)(c => c.name)
+    override def enrolling(course: Course) = _courses = append(Cons(course,Nil()), _courses)
+    override def hasTeacher(teacher: String): Boolean = contains(map(_courses)(c => c.teacher), teacher)
 
 object Course:
-  def apply(name: String, teacher: String): Course = ???
+  def apply(name: String, teacher: String): Course = CourseImpl(name, teacher)
+  private case class CourseImpl(override val name: String, override val teacher: String) extends Course
 
 @main def checkStudents(): Unit =
   val cPPS = Course("PPS", "Viroli")
