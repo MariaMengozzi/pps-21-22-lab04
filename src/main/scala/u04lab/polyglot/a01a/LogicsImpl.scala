b108d94344cfff94b0f67e79660cbe96176b2b6c
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-03-21 12:48:52 +0100

    implement exercise 3 battaglia navale

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -1,5 +1,7 @@
 package u04lab.polyglot.a01a
 import Logics.*
+import u04lab.polyglot.Pair
+
 import scala.util.Random
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/
@@ -10,11 +12,14 @@ class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
   private val random = Random(42)
   private val boatRow = random.nextInt(size)
   private val boatLeftCol = random.nextInt(size-boat+1)
+  private var hitedPosition: List[Tuple] = List()
 
   println(s"x=$boatLeftCol  y=$boatRow") //x=0 y=2
 
   def hit(row: Int, col: Int) =
-    if row == boatRow && col == boatLeftCol
-      then Result.HIT
+    if row == boatRow && col >= boatLeftCol && col < boatLeftCol+boat then
+      hitedPosition = hitedPosition.appended((row, col))
+      if hitedPosition.length == boat then Result.WON else Result.HIT
     else
-      Result.MISS
+      failures = failures + 1
+      if failures == FAILURES then Result.LOST else Result.MISS
